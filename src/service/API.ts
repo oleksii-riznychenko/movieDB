72a5fef397e373063c70ad6a2b489dd43abb0b39
Dmytro <mitrohin2120@gmail.com>
2023-06-13 14:58:06 +0300

    New Api

--- src/service/API.ts
+++ src/service/API.ts
@@ -1,6 +1,5 @@
-export const APIKEY = 'k_10zfa02h';
+export const APIKEY = 'k_sl97avk8';
 // k_sl97avk8 Dmytro 1 user12345
-// k_cta6yf13 Dmytro 2 dmytro123
 // k_10zfa02h Dmytro 3 mdm-12345
 // k_3kkf3myc Alex
 
@@ -9,4 +8,5 @@ export const APIKEY = 'k_10zfa02h';
 // k_k97u1elw Dmytro 2 -
 // k_ogwkpqpy Dmytro 3 -
 // k_fzi6xgaq Dmytro 4 -
+// k_cta6yf13 Dmytro 2 dmytro123 -
 export const baseUrl = 'https://imdb-api.com';
